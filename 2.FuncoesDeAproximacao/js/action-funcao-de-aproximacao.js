0e4c1c00c7b16efbc503c896542c7a31b9d25009
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-03-26 15:14:17 -0300

    Comecando exemplo de funcao de aproximacao

---
+++ 2.FuncoesDeAproximacao/js/action-funcao-de-aproximacao.js
@@ -0,0 +1,14 @@
+function derivada(n) {
+    return n * (1 - n);
+}
+
+function executar() {
+    $('#linhas').html('');
+
+    var input = $('#entrada').val();
+    var target = $('#busca').val();
+    var weight = Math.random();
+    var epochs = $('#epocas').val();
+
+
+}
